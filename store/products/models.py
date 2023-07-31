45af3a35ee177a8c9cf5ecdf4aac3e3604a5e6ee
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-07-31 23:47:30 +0200

    created models
    models for products

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,47 @@
+import stripe
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images', null=True, blank=True)
+    stripe_product_price_id = models.CharField(max_length=128, null=True, blank=True)
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return f'Продукт: {self.name} | Категория: {self.category.name}'
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.stripe_product_price_id:
+            stripe_product_price = self.create_stripe_product_price()
+            self.stripe_product_price_id = stripe_product_price['id']
+        super(Product, self).save(force_insert=False, force_update=False, using=None, update_fields=None)
+
+    def create_stripe_product_price(self):
+        stripe_product = stripe.Product.create(name=self.name)
+        stripe_product_price = stripe.Price.create(
+            product=stripe_product['id'], unit_amount=round(self.price * 100), currency='rub')
+        return stripe_product_price
