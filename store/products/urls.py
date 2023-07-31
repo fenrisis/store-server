9602e5d5cb4594002016ab75900c6580060b6825
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-07-31 23:50:58 +0200

    views & urls for products

---
+++ store/products/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from products.views import ProductsListView
+app_name = 'products'
+
+urlpatterns = [
+    path('', ProductsListView.as_view(), name='index'),
+    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
+    path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
+
+
+]
