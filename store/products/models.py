41054fa39aedc6bb90ccd8fb619ba9e239e1d6a7
Leonid3# <116304748+fenrisis@users.noreply.github.com>
2023-08-08 14:24:47 +0200

    create views for orders

--- store/products/models.py
+++ store/products/models.py
@@ -2,6 +2,7 @@ import stripe
 from django.conf import settings
 from django.db import models
 
+from users.models import User
 
 
 
@@ -45,3 +46,59 @@ class Product(models.Model):
         stripe_product_price = stripe.Price.create(
             product=stripe_product['id'], unit_amount=round(self.price * 100), currency='rub')
         return stripe_product_price
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+    def stripe_products(self):
+        line_items = []
+        for basket in self:
+            item = {
+                'price': basket.product.stripe_product_price_id,
+                'quantity': basket.quantity,
+            }
+            line_items.append(item)
+        return line_items
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
+
+    def sum(self):
+        return self.product.price * self.quantity
+
+    def de_json(self):
+        basket_item = {
+            'product_name': self.product.name,
+            'quantity': self.quantity,
+            'price': float(self.product.price),
+            'sum': float(self.sum()),
+        }
+        return basket_item
+
+    @classmethod
+    def create_or_update(cls, product_id, user):
+        baskets = Basket.objects.filter(user=user, product_id=product_id)
+
+        if not baskets.exists():
+            obj = Basket.objects.create(user=user, product_id=product_id, quantity=1)
+            is_created = True
+            return obj, is_created
+        else:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+            is_crated = False
+            return basket, is_crated
